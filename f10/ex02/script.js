eb865f16a88f2f733c102cbb72e805ed01ae7bec
Miguel Ferreira <miguel.correiaferreira@gmail.com>
2020-12-20 20:36:45 +0000

    Add F10

---
+++ f10/ex02/script.js
@@ -0,0 +1,23 @@
+function countWordsAndChars(words) {
+
+    // total number of words
+    let totalNumberWords = words.length
+
+    // total number of chars in all words
+    let totalCharsInWords = 0
+    for (let i in words) {
+        totalCharsInWords += words[i].length
+    }
+
+    // average number of chars per word
+    averageCharsInWords = totalCharsInWords/totalNumberWords
+
+    // output the results
+    console.log('Total number of words: ' + totalNumberWords)
+    console.log('Total number of chars: ' + totalCharsInWords)
+    console.log('Average number of chars per word: ' + averageCharsInWords)
+}
+
+let wordsArray = ['jogging', 'running', 'simming']
+console.log('Array: ' + wordsArray);
+countWordsAndChars(wordsArray)

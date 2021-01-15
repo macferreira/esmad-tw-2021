a76f933de49394b3bffd0cd25349efc29d112b4e
Miguel Ferreira <miguel.correiaferreira@gmail.com>
2021-01-15 15:02:52 +0000

    Minor fix on F11 EX03

--- f11/ex03/script.js
+++ f11/ex03/script.js
@@ -1,13 +1,15 @@
+// exercise a
 document.getElementById('nickname').innerHTML = 'John Doe'
 document.getElementById('age').innerHTML = '30'
 document.getElementById('hometown').innerHTML = 'London'
 
+// exercise b
 let items = document.getElementsByTagName('li')
-
-for (var i = 0; i < items.length; i++) {
+for (let i = 0; i < items.length; i++) {
     items[i].className = 'listitem'
 }
 
-var image = document.createElement('img')
+// exercise c
+let image = document.createElement('img')
 image.src = 'http://placekitten.com/200/300'
 document.body.appendChild(image)
